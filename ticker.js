945f15e8191dee7482b1ba159707e2a2bbc59262
Kautilya Kondragunta <kautilyakondragunta@gmail.com>
2019-06-04 12:51:34 +0530

    Added more error handling functionality

--- ticker.js
+++ ticker.js
@@ -3,12 +3,13 @@ const colors = require('colors');
 
 try{
 	var ticker = process.argv.slice(2)[0].toLowerCase();
-	var frequency = process.argv.slice(2)[1];
-	const url = "https://api.wazirx.com/api/v2/tickers";
+	var frequency = process.argv.slice(2)[1];            
+	const url = "https://api.wazirx.com/api/v2/tickers";   // API URL
 	var prev=0;
 	var totalChange;
 	var now = new Date();
 	
+	//Function to find the percentage change in price.
 	
 	function percChange(first, second) {
 		var perc = 0;
@@ -20,11 +21,13 @@ try{
 		return perc.toFixed(2) + ' %';
 	}
 	
+	
 	if(!frequency) {
-		frequency = 4000;
+		frequency = 4000;     //Default frequency
 	}
 	
-
+	// Log Info into console before getting data.
+	
 	console.log("\n\n" + now.toTimeString().grey);
 	console.log(colors.green("\n### This program shows you real time updates of the CRYPTO-TICKER you input.\n### To quit press CTRL + C"));
 	console.log(colors.green("### Mention frequency(ms) after ticker or default (4sec) will be set"));
@@ -40,41 +43,44 @@ try{
 			
 			response.on('end', ()=>{
 				body = JSON.parse(body);
-				var ticker_name = body[ticker]['name'];
-				var last_buy = body[ticker]['last'];
-				var open = body[ticker]['open'];
-				totalChange = ((last_buy-open)/open)*100;
-				totalChange = totalChange.toFixed(2);
+				if(body[ticker]) {  	// Check if the ticker provided is valid 
+					var ticker_name = body[ticker]['name'];
+					var last_buy = body[ticker]['last'];
+					var open = body[ticker]['open'];
+					totalChange = ((last_buy-open)/open)*100;
+					totalChange = totalChange.toFixed(2);
+						
+						if(totalChange > 0) {
+							console.log(`Daily change : ${colors.green(totalChange) + "%"}`);
+						} else if (totalChange < 0){
+							console.log(`Daily change : ${colors.red(totalChange) + "%"}`);
+						} else {
+							console.log("Daily Change : " + totalChange);
+						}
 					
-					if(totalChange > 0) {
-						console.log(`Daily change : ${colors.green(totalChange) + "%"}`);
-					} else if (totalChange < 0){
-						console.log(`Daily change : ${colors.red(totalChange) + "%"}`);
+					
+					if(prev != last_buy) {
+						var perc = percChange(prev, last_buy);  //Find the percentage change
+						if(prev < last_buy) {
+							console.log(`${colors.inverse(ticker_name)} ${colors.green(last_buy.bold +" "+ perc.inverse)}   ${now.toTimeString().grey}`);
+						} else if(prev > last_buy){
+							console.log(`${colors.inverse(ticker_name)} ${colors.red(last_buy  +" "+  perc.inverse)}   ${now.toTimeString().grey}`);
+						}
+						
+						prev = last_buy;
+						
 					} else {
-						console.log("Daily Change : " + totalChange);
+					console.log(`${colors.inverse(ticker_name)} ${colors.bold(last_buy)}            ${now.toTimeString().grey}`);
 					}
-				
-				
-				if(prev != last_buy) {
-					var perc = percChange(prev, last_buy);  //Find the percentage change
-					if(prev < last_buy) {
-						console.log(`${colors.inverse(ticker_name)} ${colors.green(last_buy.bold +" "+ perc.inverse)}   ${now.toTimeString().grey}`);
-					} else if(prev > last_buy){
-						console.log(`${colors.inverse(ticker_name)} ${colors.red(last_buy  +" "+  perc.inverse)}   ${now.toTimeString().grey}`);
-					}
-					
-					prev = last_buy;
-					
 				} else {
-				console.log(`${colors.inverse(ticker_name)} ${colors.bold(last_buy)}            ${now.toTimeString().grey}`);
+					clearInterval(requestLoop);     //Exit the loop
+					var error = new Error("Error: Invalid Ticker Provided");
+					console.log(colors.red(error.message));
 				}
 			});
 		});
 	}, frequency);
 	
 } catch(err) {
-	if(err.message.includes("/ENOTFOUND/")) {
-		throw new Error("Network connection not available");
-	}
-	console.log(colors.red("Message : " + err.message));
+	console.log(colors.red("Error : " + err.message));
 }
